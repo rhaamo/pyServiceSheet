956ddeaf4b9cfb5f90cb05b89377f4d41374e344
Dashie <dashie@sigpipe.me>
2020-12-29 07:43:25 +0100

    docker fix

--- docker/server.sh
+++ docker/server.sh
@@ -2,4 +2,4 @@
 python /app/manage.py collectstatic --noinput
 python /app/manage.py migrate
 cd /app
-gunicorn --log-file=- --worker-tmp-dir /dev/shm config.asgi:application -w ${WEB_WORKERS-2} --threads ${WEB_THREADS-4} -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 ${GUNICORN_ARGS-}
+gunicorn --log-file=- --worker-tmp-dir /dev/shm pyservicesheet.asgi:application -w ${WEB_WORKERS-2} --threads ${WEB_THREADS-4} -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000 ${GUNICORN_ARGS-}
